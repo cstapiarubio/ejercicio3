92ef02517d8c37399f0ca35ab58b23be9b1a72be
CarolinaTapia <cstapiarubio@gmail.com>
2017-04-24 17:00:33 -0300

    estructura basica

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+
+var temperaturaCelsius; 
+var fahrenheit;
+function calculoTemperatura (temperaturaCelsius){
+return fahrenheit=temperaturaCelsius*(9/5)+32;
+}
+
+document.write(calculoTemperatura);
